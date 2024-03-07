5342fbc81ee5f2ce50a0f2869730dc61da09a345
Deepak Pagaro <dpgit2024@gmail.com>
2024-03-07 01:47:22 -0500

    add rate limiter

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,19 @@ import shortenUrlRouter from './routes/shortenUrl'
 import { logger, requestLoggerMiddleware } from './clients/loggerClient'
 import { ValidationError } from 'express-validation'
 import helmet from 'helmet'
+import { rateLimit } from 'express-rate-limit'
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+})
 
 const app = express()
 app.use(helmet())
+app.use(limiter)
 app.use(express.json())
 app.use(requestLoggerMiddleware)
 
