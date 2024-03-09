1e6e3930d2a78d31d2624f7f8271951977540805
Deepak Pagaro <dpgit2024@gmail.com>
2024-03-08 22:47:52 -0500

    url router test simple

---
+++ tests/routes/urlRecordRouter.test.ts
@@ -0,0 +1,24 @@
+import app from '../../src/app'
+import request from 'supertest'
+
+
+jest.mock('../../src/clients/loggerClient')
+jest.mock('mongoose')
+jest.createMockFromModule('../../src/model/miniUrlModel')
+jest.mock('../../src/lib/database')
+jest.mock('../../src/controllers/urlRecordController')
+
+const agent = request(app)
+
+
+
+describe('urlRecordRouter tests - ',function() {
+    afterAll(() => {
+        jest.clearAllMocks()
+    })
+    it('should return 400 for missing query param',async function() {
+        const res = await agent.get('/url-record')
+        
+        expect(res.statusCode).toBe(400)
+    })
+})
