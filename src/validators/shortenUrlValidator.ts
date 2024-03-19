f96e7880fc6e5254d8aff0885df17b055a3efe02
Deepak Pagaro <dpgit2024@gmail.com>
2024-03-19 15:47:29 -0400

    limit character validation

--- src/validators/shortenUrlValidator.ts
+++ src/validators/shortenUrlValidator.ts
@@ -3,10 +3,10 @@ import { urlValueValidator } from '../utils/helpers'
 
 export const shortenUrlScema = {
     headers: Joi.object({
-        'x-correlation-id': Joi.string().trim().required()
+        'x-correlation-id': Joi.string().max(1000).trim().required()
     }).unknown(true),
     body: Joi.object({
-        url: Joi.string().trim().required()
+        url: Joi.string().max(1000).trim().required()
     })
         .custom(urlValueValidator)
 }
