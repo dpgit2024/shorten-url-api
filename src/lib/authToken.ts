56ebd4df57fb4a9c17caac2181ebfda2e2db4455
Deepak Pagaro <dpgit2024@gmail.com>
2024-03-19 14:19:32 -0400

    fix error response invalid token

--- src/lib/authToken.ts
+++ src/lib/authToken.ts
@@ -1,3 +1,4 @@
+import { logger } from '../clients/loggerClient'
 import { config } from '../../config/config'
 import jwt from 'jsonwebtoken'
 
@@ -8,7 +9,11 @@ export const generateToken = function (id: string): string {
 }
 
 export const validateToken = function (token: string): string | undefined {
-    const decoded = jwt.verify(token, config.JWT.SECRET) as { userName: string }
-    const userName = decoded?.userName
-    return userName
+    try {
+        const decoded = jwt.verify(token, config.JWT.SECRET) as { userName: string }
+        const userName = decoded?.userName
+        return userName
+    } catch(error) {
+        logger.error(error)
+    }
 }
--- tests/lib/authToken.test.ts
+++ tests/lib/authToken.test.ts
@@ -1,10 +1,12 @@
 import jwt from 'jsonwebtoken'
 import { generateToken, validateToken } from '../../src/lib/authToken'
-
+import { logger } from '../../src/clients/loggerClient'
 jest.mock('jsonwebtoken')
+jest.mock('../../src/clients/loggerClient')
 
 const mockSign = jwt.sign as jest.Mock
 const mockVerify = jwt.verify as jest.Mock
+const mockLoggerError = logger.error as jest.Mock
 
 describe('authToken tests -', function() {
     it('generateToken - should call sign', function() {
@@ -20,5 +22,15 @@ describe('authToken tests -', function() {
         mockVerify.mockReturnValueOnce({userName: mockUserId})
         const value = validateToken(arg)
         expect(value).toBe(mockUserId)
+    })
+
+    it('validateToken - should call error logger', function() {
+        const arg = 'fakehjhhjhj'
+        mockVerify.mockImplementationOnce(() => {
+            throw new Error()
+        })
+        const value = validateToken(arg)
+        expect(mockLoggerError).toHaveBeenCalled()
+        expect(value).not.toBeDefined()
     })
 })
