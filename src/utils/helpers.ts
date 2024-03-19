a4d703d70c51a370aee87c4652b83e667d76f622
Deepak Pagaro <dpgit2024@gmail.com>
2024-03-18 22:11:13 -0400

    format file utils

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,13 +1,13 @@
 import { nanoid } from 'nanoid'
 import isUrl from 'is-url'
 
-export const generateMiniUrlId = function(length = 5): string {
-    return  nanoid(length)
+export const generateMiniUrlId = function (length = 5): string {
+    return nanoid(length)
 }
 
-export const urlValueValidator = function(value: {url: string}, helpers: any) {
-    if(!isUrl(value.url)) {
-        return helpers.error('any.invalid', { message: 'Invalid URL.'})
+export const urlValueValidator = function (value: { url: string }, helpers: any) {
+    if (!isUrl(value.url)) {
+        return helpers.error('any.invalid', { message: 'Invalid URL.' })
     }
     return value
 }
